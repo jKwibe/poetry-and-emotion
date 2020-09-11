6cdc232c1aa67f4ae13a9b57c60232ef21abbf73
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-11 09:27:48 -0600

    Add test to search for poems.

--- spec/features/user_can_visit_root_path_spec.rb
+++ spec/features/user_can_visit_root_path_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
-describe "User can visit root path" do
-  it "to see a search form" do
+describe 'User can visit root path' do
+  it 'to see a search form' do
     visit '/'
 
     expect(current_path).to eq(root_path)
     expect(page).to have_content('Welcome to Poetry and Emotion')
     expect(page).to have_selector('input')
+  end
+
+  it 'can search a poem' do
+    visit root_path
+
+    fill_in :author, with: 'Emily'
+    click_button 'Get Poems'
+    expect(current_path).to eq('/search')
   end
 end
